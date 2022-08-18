0e1aa194cc7ebde09bb4911c8c7ab96916363a0a
Tobias Badertscher <tobbad@users.noreply.github.com>
2022-08-18 12:38:01 +0200

    Added CMake Project

--- Loesungen/test.cpp
+++ Loesungen/test.cpp
@@ -1,10 +1,9 @@
 // basic file operations
 #include <iostream>
-#include <fstream>
-#include "version.h"
+//#include "version.h"
 using namespace std;
 
 int main (int argc, char *argv[]) {
-  myfile << "Hello world!\n";
+  std::cout << "Hello world!\n";
   return 0;
 }

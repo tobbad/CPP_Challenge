0f04346f62ff4feb713db7bbd8a8de74c35ba250
Tobias Badertscher <tobbad@users.noreply.github.com>
2022-08-18 11:51:59 +0200

    WIP.
    WIP.

---
+++ Loesungen/test.cpp
@@ -0,0 +1,10 @@
+// basic file operations
+#include <iostream>
+#include <fstream>
+#include "version.h"
+using namespace std;
+
+int main (int argc, char *argv[]) {
+  myfile << "Hello world!\n";
+  return 0;
+}

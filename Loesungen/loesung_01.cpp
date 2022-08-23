0184205d3415762d7bf15325f1d0c72e14ab96be
Tobias Badertscher <tobbad@users.noreply.github.com>
2022-08-23 16:50:34 +0200

    Remove build folder

--- Loesungen/loesung_01.cpp
+++ Loesungen/loesung_01.cpp
@@ -1,39 +1,35 @@
 // basic file operations
 #include <iostream>
 #include "getopt.h"
-#include <unistd.h>
-#include "version.h"
+//#include "version.h"
 using namespace std;
 
 int main (int argc, char *argv[]) {
-	int c;
-	int aflag, bflag, cvalue;
-	while ((c = getopt (argc, argv, "abc:")) != -1)
-    switch (c) {
-      case 'a':
-        aflag = 1;
-        break;
-      case 'b':
-        bflag = 1;
-        break;
-      case 'c':
-        cvalue = optarg;
-        break;
-      case '?':
-        if (optopt == 'c')
-          fprintf (stderr, "Option -%c requires an argument.\n", optopt);
-        else if (isprint (optopt))
-          fprintf (stderr, "Unknown option `-%c'.\n", optopt);
-        else
-          fprintf (stderr,
-                   "Unknown option character `\\x%x'.\n",
-                   optopt);
-        return 1;
-      default:
-        abort ();
-	}
+	int summe;
+	int limite;
+    int flags, opt;
+   int nsecs, tfnd;
+
+   nsecs = 0;
+   tfnd = 0;
+   flags = 0;
+   while ((opt = getopt(argc, argv, "nt:")) != -1) {
+	   switch (opt) {
+	   case 'n':
+		   flags = 1;
+		   break;
+	   case 't':
+		   nsecs = atoi(optarg);
+		   tfnd = 1;
+		   break;
+	   default: /* '?' */
+		   fprintf(stderr, "Usage: %s [-t nsecs] [-n] name\n",
+				   argv[0]);
+		   exit(EXIT_FAILURE);
+	   }
+   }
 	for(int i=obtind; i<argc; i++){     
-		std::stdout << argv[i] << std::endl;
+		std::cout << argv[i] << std::endl;
 	}
 
  std::cout << "Summe aller durch 3 und 5 teilbahren Zahlen bis zu einer Anwender vorgegeben Limite\n";

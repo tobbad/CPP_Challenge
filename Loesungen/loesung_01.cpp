5a4cc99a081393368c2153c69129f50cff1bed65
Tobias Badertscher <tobbad@users.noreply.github.com>
2022-08-25 08:39:31 +0200

    Remove usage of getopt and get limit from command line

--- Loesungen/loesung_01.cpp
+++ Loesungen/loesung_01.cpp
@@ -1,48 +1,23 @@
 // basic file operations
 #include <iostream>
-#include "getopt.h"
 //#include "version.h"
 using namespace std;
 
 int main (int argc, char *argv[]) {
-	int summe;
+	long long int summe=0;
 	int limite;
-    int flags, opt;
-   int nsecs, tfnd;
-
-   nsecs = 0;
-   tfnd = 0;
-   flags = 0;
-   while ((opt = getopt(argc, argv, "nt:")) != -1) {
-	   switch (opt) {
-	   case 'n':
-		   flags = 1;
-		   break;
-	   case 't':
-		   nsecs = atoi(optarg);
-		   tfnd = 1;
-		   break;
-	   default: /* '?' */
-		   fprintf(stderr, "Usage: %s [-t nsecs] [-n] name\n",
-				   argv[0]);
-		   exit(EXIT_FAILURE);
-	   }
-   }
-	for(int i=obtind; i<argc; i++){     
-		std::cout << argv[i] << std::endl;
-	}
 
  std::cout << "Summe aller durch 3 und 5 teilbahren Zahlen bis zu einer Anwender vorgegeben Limite\n";
   std::cout << "Limite " ;
   std::cin >> limite;
   std::cout << "Summiere bis " << limite << endl;
-  for (int i=0;i<limite;i++)
+  for (int i=0;i<limite+1;i++)
   {
 	  if ((i%3)==0 || (i%5)==0)
 	  {
 		  summe+=i;
 	  }
-	  std::cout << i << " "<< summe<< std::endl;
+	  //std::cout << i << " "<< summe<< std::endl;
 	  
   }
   std::cout << "Result is " << summe;

a834ecf4793590f7f8cdaf8f72646b6def5ad8f9
Tobias Badertscher <tobbad@users.noreply.github.com>
2022-08-25 09:03:19 +0200

    Add Python solution+

---
+++ Loesungen/loesung_01.py
@@ -0,0 +1,14 @@
+
+def loesung():
+    print("Yes I am done")
+    limit=input("Upper limit? ")
+    print(limit)
+    limit=int(limit)
+    summe=0
+    for i in range(limit+1):
+        if (i%3==0) or (i%5==0):
+            summe += i
+    print("Result is %d" % summe)
+
+if __name__ == "__main__":
+    loesung()

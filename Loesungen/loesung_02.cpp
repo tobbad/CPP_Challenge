3cc034ae0012506122962a0b87c8aac05574ae67
Badertscher Tobias <tobias.badertscher@noser.com>
2022-08-29 10:56:31 +0100

    Solution for Problem 2

--- Loesungen/loesung_02.cpp
+++ Loesungen/loesung_02.cpp
@@ -5,12 +5,12 @@
 using namespace std;
 
 int gcd(int a, int b=0) {
-	std::cout << "a: " << a << " b: " << b << std::endl;
+	//std::cout << "a: " << a << " b: " << b << std::endl;
 	if (b == 0) {
-		std::cout << "\tReturn \'" << a << "\'" <<std::endl;
+		//std::cout << "\tReturn \'" << a << "\'" <<std::endl;
 		return gcd(a);
 	} else {
-		std::cout << "\tReturn \'" << a % b << "\'" << std::endl;
+		//std::cout << "\tReturn \'" << a % b << "\'" << std::endl;
 		return gcd(b, a % b);
 	}
 }
@@ -19,14 +19,12 @@ int main (int argc, char *argv[]) {
 	long long int summe=0;
 	int a,b, res=0;
 
-   std::cout << "Keinster gemeinsamer Teiler\n";
+   std::cout << "Keinster gemeinsamer Teiler\n" << std::endl;
    std::cout << "a: ";
    std::cin >> a;
    std::cout << "b: ";
    std::cin >> b;
    res = gcd(a, b);
-   std::cout << "Result is: " << res;
-
-
+   std::cout << "Result is: " << res << std::endl;
   return 0;
 }

28c01b9fc4148fd06c5d135aee1e45f08f9365bd
Tobias Badertscher <badi@baerospace.ch>
2022-10-18 10:01:06 +0100

    Add solution in rust for problem 8

---
+++ Loesungen/rust/loesung_08/src/main.rs
@@ -0,0 +1,8 @@
+use std::io::*;
+use std::process::*;
+use loesung_08::{get_int_fromcl, print_narcissistics1};
+
+fn main(){
+	//let limit = get_int_fromcl(&String::from("Upper limit? "));
+	print_narcissistics1();
+}

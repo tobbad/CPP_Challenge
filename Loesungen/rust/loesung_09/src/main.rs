14813551a3a405ad44a8e67173175e1abd1ea2d4
Tobias Badertscher <badi@baerospace.ch>
2022-10-20 15:35:02 +0100

    Added source for problem 9

---
+++ Loesungen/rust/loesung_09/src/main.rs
@@ -0,0 +1,9 @@
+use std::io::*;
+use std::process::*;
+use loesung_09::{get_int_fromcl, prime_factors};
+
+fn main(){
+	let mut limit = get_int_fromcl(&String::from("Upper limit? "));
+	let res = prime_factors(limit);
+	println!("{:?}",res);
+}

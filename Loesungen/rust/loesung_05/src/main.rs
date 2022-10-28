94a7bbbab9d04099838df63e4b2969bfab752595
Tobias Badertscher <badi@baerospace.ch>
2022-10-28 16:41:57 +0200

    Added solution 5.

---
+++ Loesungen/rust/loesung_05/src/main.rs
@@ -0,0 +1,14 @@
+use std::io::*;
+use loesung_05::{get_int_fromcl, is_prim};
+
+fn main(){
+	let limit = get_int_fromcl(&String::from("Upper limit? "));
+	for i in (0..limit+1)	{
+		if is_prim(i) || is_prim(i+6)
+		{
+			println!("{};{}",i, i+6);
+			//exit(0);
+		}
+		
+	}
+}

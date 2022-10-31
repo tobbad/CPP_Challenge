75bcd5e2d9427fb21539cc90a9932fece4e3338c
Tobias Badertscher <badi@baerospace.ch>
2022-10-31 11:40:03 +0100

    Proper setting tabs in example solution

--- CPP_Challenge/chapter01/problem_12/main.cpp
+++ CPP_Challenge/chapter01/problem_12/main.cpp
@@ -31,33 +31,31 @@ std::pair<unsigned long long, long> longest_collatz_uncached(unsigned long long 
 
 std::pair<unsigned long long, long> longest_collatz(unsigned long long const limit)
 {
-   long length = 0;
-   unsigned long long number = 0;
+    long length = 0;
+    unsigned long long number = 0;
 
-   std::vector<int> cache(limit + 1, 0);
+    std::vector<int> cache(limit + 1, 0);
 
-   for (unsigned long long i = 2; i <= limit; i++) 
-   {
-      auto n = i;
-      long steps = 0;
-      while (n != 1 && n >= i) 
-      {
-         if ((n % 2) == 0) 
-            n = n / 2;
-         else 
-            n = n * 3 + 1;
-         steps++;
-      }
-      cache[i] = steps + cache[n];
+    for (unsigned long long i = 2; i <= limit; i++) 
+    {
+        auto n = i;
+        long steps = 0;
+        while (n != 1 && n >= i) 
+        {
+            if ((n % 2) == 0) n = n / 2;
+            else n = n * 3 + 1;
+            steps++;
+        }
+        cache[i] = steps + cache[n];
 
-      if (cache[i] > length) 
-      {
-         length = cache[i];
-         number = i;
-      }
-   }
+        if (cache[i] > length) 
+        {
+            length = cache[i];
+            number = i;
+        }
+    }
 
-   return std::make_pair(number, length);
+    return std::make_pair(number, length);
 }
 
 int main()

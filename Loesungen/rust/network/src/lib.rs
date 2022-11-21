7bdbe7ada19f9029949c935b87fe812164917a8a
Tobias Badertscher <badi@baerospace.ch>
2022-11-21 11:19:51 +0000

    Work in progress

--- Loesungen/rust/network/src/lib.rs
+++ Loesungen/rust/network/src/lib.rs
@@ -7,12 +7,6 @@ pub struct IPV4
     ip_u32: u32,
 }
 
-enum input {
-    String,
-    [u8;4],
-}
-
-
 pub fn string_ip_v4(string:String) -> [u8;4]
 {   let ss = string.split(".");
 	let mut res: [u8;4]= [0,0,0,0];
@@ -29,31 +23,22 @@ pub fn string_ip_v4(string:String) -> [u8;4]
 
 impl IPV4
 {
-    // pub fn new<>(new_ip: [u8;4])->  IPV4 
-    // {
-        // let ipu32= new_ip[0]<<24 + new_ip[1]<<16+ new_ip[2]<<8 + new_ip[3];
-        // IPV4 {
-            // ip: new_ip,
-            // ipu32 : ipu32
-        // }
-    // }
-    
-    pub fn new(ip_str :String)->  IPV4 
+    pub fn new<>(new_ip:[u8;4])->  IPV4 
     {
-        let new_ip = string_ip_v4(ip_str);
-        let new_ip_u32 = u32::from_be_bytes(new_ip);
-        IPV4 {
+        let ipu32= new_ip[0]>>24 + new_ip[1]>>16+ new_ip[2]>>8 + new_ip[3];
+        let IPV4 = {
             ip: new_ip,
-            ip_u32 : new_ip_u32,
+            ip_u32 : ipu32
         }
     }
+    
 }
 
 impl Display for IPV4
 {
     fn fmt(&self, f: &mut std::fmt::Formatter,) -> std::fmt::Result
     {
-        write!(f, "IP: {}.{}.{}.{} of {}", self.ip[0], self.ip[1], self.ip[2],self.ip[3], self.type_of())
+        write!(f, "IP: {}.{}.{}.{}", self.ip[0], self.ip[1], self.ip[2],self.ip[3])
     }
  
 }

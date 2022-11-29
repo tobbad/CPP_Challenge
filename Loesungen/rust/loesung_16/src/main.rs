205e5bf2a8eef9db9697cf960cc72903e3c7a881
Tobias Badertscher <badi@baerospace.ch>
2022-11-29 11:30:31 +0000

    Work in Progress

--- Loesungen/rust/loesung_16/src/main.rs
+++ Loesungen/rust/loesung_16/src/main.rs
@@ -3,12 +3,17 @@ use lib::{get_input};
 
 fn main(){
     let ps  = get_input("IP 1: "); // This is a String
-    let ipa = string_ip_v4(ps);
-    let ip1 = IPV4::new(ipa);
+    let ipa = string_to_u32(ps);
+    let ip1 = IPV4::new(ipn);
     let ps  = get_input("IP 2: "); // This is a String
-    let ipa = string_ip_v4(ps);
+    let ipa = string_to_u32(ps);
     let ip2 = IPV4::new(ipa);
     println!("{:?} to {:?}", ip1, ip2);
+    for i in ip1.to_u32()..ip2.to_u32()
+    {
+        ip = IPV4::new(i);
+        println!("{}", ip);
+    }
     println!("{} {}", ip1, ip2);
 
 }
--- Loesungen/rust/network/src/lib.rs
+++ Loesungen/rust/network/src/lib.rs
@@ -1,9 +1,8 @@
 use std::fmt::{Display};
-
+use std::fmt::Error;
 #[derive(Debug)]
 pub struct IPV4
 {
-    ip : [u8;4],
     ip_u32: u32,
 }
 
@@ -21,27 +20,52 @@ pub fn string_ip_v4(string:String) -> [u8;4]
     res
 }
 
+pub fn string_to_u32(ip_str:String) -> u32
+{
+    let ipu8 =string_ip_v4(ip_str);
+    u32::from_be_bytes(ipu8)
+}
+
 impl IPV4
 {
-    pub fn new(new_ip:[u8;4])->  IPV4 
+    pub fn new(new_ip:u32)->  IPV4 
     {
-        let ip_u32_new:u32= u32::from_be_bytes(new_ip);
         IPV4{
-            ip: new_ip,
-            ip_u32 : ip_u32_new
+            ip_u32 : new_ip
         }
     }
+    
+    pub fn steps_between(start:&Self, end:&Self) -> Result< (),u32> 
+    {
+        if  end.ip_u32 > start.ip_u32
+        {
+             end.ip_u32- start.ip_u32
+        } else {
+            Err("No Proper oder")
+        }
+    }
+    
     pub fn to_u32(self:&IPV4) -> u32 {
         self.ip_u32
     }
     
+    pub fn to_u8_4(self:&IPV4) -> [u8;4]
+    {
+        let mut res: [u8;4]= [0,0,0,0];
+        for i in 0..3
+        {
+            res[i] = (self.ip_u32>>(3*8-i)) as u8;
+        }
+        res
+    }
 }
 
 impl Display for IPV4
 {
     fn fmt(&self, f: &mut std::fmt::Formatter,) -> std::fmt::Result
-    {
-        write!(f, "IP: {}.{}.{}.{} ({})", self.ip[0], self.ip[1], self.ip[2],self.ip[3], self.ip_u32)
+    {   
+        let ip = self.to_u8_4();
+        write!(f, "IP: {}.{}.{}.{} ({})", ip[0], ip[1], ip[2], ip[3], self.ip_u32)
     }
  
 }
